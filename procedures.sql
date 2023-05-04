bec238e17c0191af2cc19efa188deb7d4eab4be8
Angel <54374165+Coolacho@users.noreply.github.com>
2023-05-04 13:59:17 +0300

    Added procedures and some triggers

--- procedures.sql
+++ procedures.sql
@@ -1,5 +1,8 @@
 USE restaurant_management;
 
+#..........................................................................................
+
+#Procedure to fill the shifts table with the default 4, 6, 8, 12 hour shifts
 DROP PROCEDURE IF EXISTS fill_shifts;
 DELIMITER |
 CREATE PROCEDURE fill_shifts (IN start_date DATETIME)
@@ -28,6 +31,7 @@ CALL fill_shifts('2023-02-01 10:00:00');
 
 #..........................................................................................
 
+#Calculates the bill on certain reservation by taking an array of products' ids
 DROP PROCEDURE IF EXISTS calculate_bill;
 DELIMITER |
 CREATE PROCEDURE calculate_bill (IN reservation_id INT, IN products_array JSON)
@@ -55,6 +59,11 @@ BEGIN
     
     SELECT * FROM temp_products; #for test purposes
     SELECT SUM(price) INTO bill FROM temp_products;
+    
+    SET bill = bill - (SELECT avance_amount
+						FROM reservations
+                        WHERE reservations.id = reservation_id); #Substract the avance from the bill
+    
     INSERT INTO bills (reservation_id, amount)
     VALUES (reservation_id, bill);
     
@@ -67,3 +76,282 @@ DELIMITER ;
 CALL calculate_bill(1, '[1, 1, 2, 3]');
 
 #..........................................................................................
+
+#Procedure that associates the waiter with least amount of tables taken for a shift with a reservation
+DROP PROCEDURE IF EXISTS add_waiter_to_reservation;
+DELIMITER |
+CREATE PROCEDURE add_waiter_to_reservation(IN reservation_date DATETIME, OUT waiter_id INT)
+BEGIN
+	SELECT worker_id INTO waiter_id
+    FROM (SELECT worker_id
+			FROM workers_shifts
+			JOIN shifts ON
+			workers_shifts.shift_id = shifts.id
+			JOIN workers ON
+			workers_shifts.worker_id = workers.id
+			JOIN worker_type ON
+			workers.type_id = worker_type.id
+			WHERE (reservation_date BETWEEN shifts.start_time AND shifts.end_time)
+			AND (worker_type.type = 'waiter')) AS waiters_on_shift 
+			WHERE waiters_on_shift.reservations_taken = (SELECT MIN(reservations_taken) FROM waiters_on_shift);
+END
+|
+DELIMITER ;
+
+#..........................................................................................
+
+#Function that calculates the work hours of a worker by given month and year
+#It's used in the following procedure that calculates salaries
+DROP FUNCTION IF EXISTS calculate_work_hours;
+DELIMITER |
+CREATE FUNCTION calculate_work_hours(worker_id INT, month INT, year INT)
+RETURNS INT
+READS SQL DATA
+BEGIN
+	DECLARE work_hours INT;
+    DECLARE temp_start_time DATETIME;
+    DECLARE temp_end_time DATETIME;
+    DECLARE finished INT;
+    DECLARE shifts_taken_cursor CURSOR FOR
+    SELECT start_time, end_time
+    FROM shifts
+    JOIN worker_shifts
+    ON shifts.id = worker_shifts.shift_id
+    WHERE worker_shifts.worker_id = worker_id
+    AND MONTH(shifts.start_time) = month
+    AND YEAR(shifts.start_time) = year;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+    
+    SET work_hours = 0;
+    SET finished = 0;
+    
+    OPEN shifts_taken_cursor;
+    shifts_taken_loop: WHILE(finished = 0)
+						DO
+							FETCH shifts_taken_cursor INTO temp_start_time, temp_end_time;
+                            IF (finished = 1)
+                            THEN LEAVE shifts_taken_loop;
+                            END IF;
+                            SET work_hours = work_hours + (HOUR(temp_end_time)-HOUR(temp_start_time));
+						END WHILE;
+	CLOSE shifts_taken_cursor;
+	SET finished = 0;
+    RETURN work_hours;
+    
+END
+|
+DELIMITER ;
+
+#Procedure that calculates salaries
+DROP PROCEDURE IF EXISTS calculate_salaries;
+DELIMITER |
+CREATE PROCEDURE calculate_salaries (IN month INT, IN year INT)
+BEGIN
+	DECLARE temp_worker INT;
+    DECLARE temp_hour_pay FLOAT;
+    DECLARE temp_work_hours INT;
+	DECLARE temp_salary FLOAT;
+    DECLARE issue_date DATE;
+    DECLARE finished INT;
+    DECLARE worker_cursor CURSOR FOR
+    SELECT id, hour_pay
+    FROM workers;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+    
+    SET temp_salary = 0;
+    SET issue_date = STR_TO_DATE(CONCAT(year,'-',month,'-',01),'%Y-%m-%d');
+    SET finished = 0;
+    
+    OPEN worker_cursor;
+    worker_loop: WHILE (finished = 0)
+				DO
+                FETCH worker_cursor INTO temp_worker, temp_hour_pay;
+                IF (finished = 1)
+                THEN LEAVE worker_loop;
+                END IF;
+                SELECT calculate_work_hours(temp_worker, month, year) INTO temp_work_hours;
+                SET temp_salary = temp_work_hours*temp_hour_pay;
+                IF (work_hours < 120) #Check if the worker has met the norm for worked hours
+                THEN SET temp_salary = temp_salary*0.5; #If not then pay them half the salary
+                INSERT INTO salaries (issue_date, amount, worker_id)
+                VALUES (issue_date, temp_salary, temp_worker)
+                ON DUPLICATE KEY UPDATE
+                amount = temp_salary;
+                END IF;
+                END WHILE;
+	CLOSE worker_cursor;
+    SET finished = 0;
+END
+|
+DELIMITER ;
+
+#..........................................................................................
+
+DROP PROCEDURE IF EXISTS pay_salaries;
+DELIMITER |
+CREATE PROCEDURE pay_salaries(IN month INT, IN year INT)
+BEGIN
+	DECLARE temp_salary_id INT;
+	DECLARE salaries_to_be_payed INT;
+    DECLARE salaries_payed INT;
+    DECLARE finished INT;
+    DECLARE salary_cursor CURSOR FOR
+    SELECT id, amount
+    FROM salaries
+    WHERE MONTH(issue_date) = month
+    AND YEAR(issue_date) = year
+    AND is_payed = FALSE;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+    
+    SET finished = 0;
+    
+    SELECT COUNT(*) INTO salaries_to_be_payed
+    FROM salaries
+    WHERE MONTH(issue_date) = month
+    AND YEAR(issue_date) = year
+    AND is_payed = FALSE;
+
+	OPEN salary_cursor;
+	START TRANSACTION;
+    salary_loop: WHILE (finished = 0)
+					DO
+                    FETCH salary_cursor INTO temp_salary_id;
+                    IF (finished = 1)
+                    THEN LEAVE salary_loop;
+                    END IF;
+                    INSERT INTO salary_payments (date_payed, salary_id)
+                    VALUES (DATE(NOW()), temp_salary_id);
+                    UPDATE salaries
+                    SET is_payed = TRUE
+                    WHERE salary.id = temp_salary_id;
+					END WHILE;
+	CLOSE salary_cursor;
+    SET finished = 0;
+    
+	SELECT COUNT(*) INTO salaries_payed
+    FROM salary_payments
+    WHERE date_payed = DATE(NOW());
+    
+    IF (salaries_to_be_payed = salaries_payed)
+    THEN COMMIT;
+    ELSE ROLLBACK;
+    END IF;
+END
+|
+DELIMITER ;
+
+#..........................................................................................
+
+DROP PROCEDURE IF EXISTS pay_taxes;
+DELIMITER |
+CREATE PROCEDURE pay_taxes(IN month INT, IN year INT)
+BEGIN
+	DECLARE temp_tax_id INT;
+	DECLARE taxes_to_be_payed INT;
+    DECLARE taxes_payed INT;
+    DECLARE finished INT;
+    DECLARE tax_cursor CURSOR FOR
+    SELECT id, amount
+    FROM taxes
+    WHERE MONTH(issue_date) = month
+    AND YEAR(issue_date) = year
+    AND is_payed = FALSE;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+    
+    SET finished = 0;
+    
+    SELECT COUNT(*) INTO taxes_to_be_payed
+    FROM taxes
+    WHERE MONTH(issue_date) = month
+    AND YEAR(issue_date) = year
+    AND is_payed = FALSE;
+
+	OPEN tax_cursor;
+	START TRANSACTION;
+    tax_loop: WHILE (finished = 0)
+					DO
+                    FETCH tax_cursor INTO temp_tax_id;
+                    IF (finished = 1)
+                    THEN LEAVE tax_loop;
+                    END IF;
+                    INSERT INTO tax_payments (date_payed, tax_id)
+                    VALUES (DATE(NOW()), temp_tax_id);
+                    UPDATE taxes
+                    SET is_payed = TRUE
+                    WHERE tax.id = temp_tax_id;
+					END WHILE;
+	CLOSE tax_cursor;
+    SET finished = 0;
+    
+	SELECT COUNT(*) INTO taxes_payed
+    FROM tax_payments
+    WHERE date_payed = DATE(NOW());
+    
+    IF (taxess_to_be_payed = taxes_payed)
+    THEN COMMIT;
+    ELSE ROLLBACK;
+    END IF;
+END
+|
+DELIMITER ;
+
+#..........................................................................................
+
+DROP PROCEDURE IF EXISTS calculate_income;
+DELIMITER |
+CREATE PROCEDURE calculate_income(IN month INT, IN year INT)
+BEGIN
+    DECLARE temp_amount FLOAT;
+    DECLARE income FLOAT;
+    DECLARE finished INT;
+    
+    DECLARE bill_cursor CURSOR FOR
+    SELECT amount
+    FROM bills
+    JOIN reservations
+    ON bills.reservation_id = reservations.id
+    WHERE MONTH(reservations.date) = month
+    AND YEAR(reservations.date) = year;
+    
+    DECLARE tax_cursor CURSOR FOR
+    SELECT amount
+    FROM taxes
+    JOIN tax_payments
+    ON taxes.id = tax_payments.tax_id
+    WHERE MONTH(date_payed) = month
+    AND YEAR(date_payed) = year;
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
+    
+    SET income = 0;
+    
+    OPEN bill_cursor;
+    bill_loop: WHILE (finished = 0)
+				DO
+                FETCH bill_cursor INTO temp_amount;
+                IF (finished = 1)
+                THEN LEAVE bill_loop;
+                END IF;
+                SET income = income + temp_amount;
+                END WHILE;
+	CLOSE bill_cursor;
+    SET finished = 0;
+    
+    OPEN tax_cursor;
+    tax_loop: WHILE (finished = 0)
+				DO
+                FETCH tax_cursor INTO temp_amount;
+                IF (finished = 1)
+                THEN LEAVE tax_loop;
+                END IF;
+                SET income = income - temp_amount;
+                END WHILE;
+	CLOSE tax_cursor;
+    SET finished = 0;
+    
+    INSERT INTO income (issue_date, amount)
+    VALUES(STR_TO_DATE(CONCAT(year,'-',month,'-',01),'%Y-%m-%d'), income);
+    
+END
+|
+DELIMITER ;
--- triggers_events.sql
+++ triggers_events.sql
@@ -1,25 +1,37 @@
 USE restaurant_management;
 
-DROP TRIGGER IF EXISTS append_table_waiter;
+#..........................................................................................
+
+SET GLOBAL event_scheduler = ON;
+DROP EVENT IF EXISTS fill_shifts_event;
 DELIMITER |
-CREATE TRIGGER append_table_waiter BEFORE INSERT ON reservations
+CREATE EVENT fill_shifts_event
+ON SCHEDULE EVERY 1 MONTH
+STARTS '2023-05-01 07:15:00'
+DO
+BEGIN
+CALL fill_shifts(CONCAT(DATE(NOW())+' 11:00:00'));
+END |
+DELIMITER ;
+
+#..........................................................................................
+
+DROP TRIGGER IF EXISTS check_reservation_info;
+DELIMITER |
+CREATE TRIGGER check_reservation_info BEFORE INSERT ON reservations
 FOR EACH ROW
 BEGIN
-    SET NEW.waiter_id = (SELECT worker_id
-						 FROM (SELECT worker_id
-								FROM workers_shifts
-								JOIN shifts ON
-								workers_shifts.shift_id = shifts.id
-								JOIN workers ON
-								workers_shifts.worker_id = workers.id
-								JOIN worker_type ON
-								workers.type_id = worker_type.id
-								WHERE (NEW.date BETWEEN shifts.start_time AND shifts.end_time)
-								AND (worker_type.type = 'waiter')) AS waiters_on_shift 
-						 WHERE waiters_on_shift.num_of_tables = (SELECT MIN(num_of_tables) FROM waiters_on_shift));
-                         
+	#Choose a worker that is a waiter and is on shift when the reservation starts and has the least amount of taken tables
+    CALL add_waiter_to_reservation(NEW.date, NEW.waiter_id);
+    
+    #Check if there is no waiter selected and terminate the insert.
+    IF (ISNULL(NEW.waiter_id))
+    THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Can't associate waiter to the reservation. No waiters on shift.";
+    END IF;
+    
+    #Update the amount of taken tables by the choosen waiter
 	UPDATE workers_shifts
-    SET num_of_tables = num_of_tables + 1
+    SET reservations_taken = reservations_taken + 1
     WHERE worker_id = (SELECT worker_id
 						FROM workers_shifts
 						JOIN shifts ON
@@ -27,8 +39,59 @@ BEGIN
 						WHERE (NEW.date BETWEEN shifts.start_time AND shifts.end_time)
 						AND worker_id = NEW.waiter_id);
     
-    SET NEW.table_id = (SELECT id
-						FROM tables);#FINISH THE TRIGGER!!!
+    IF (NEW.num_of_chairs > NEW.num_of_people)
+    THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Number of chairs can't be bigger than the number of people";
+    END IF;
+    
+    #Insert the reservation with pending table to appoint
+    INSERT INTO tables_reservations (reservation_id)
+    VALUES (NEW.id);
+    
 END
 |
 DELIMITER ;
+
+#..........................................................................................
+
+DROP TRIGGER IF EXISTS check_table_outdoor;
+DELIMITER |
+CREATE TRIGGER check_table_outdoor BEFORE UPDATE ON tables_reservations
+FOR EACH ROW
+BEGIN
+	IF (SELECT is_smoking
+		FROM reservations
+        WHERE reservations.id = OLD.reservation_id)
+	THEN IF NOT (SELECT is_outdoor
+			FROM tables
+            WHERE tables.id = NEW.table_id)
+		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Table must be outdoors for smokers!';
+        END IF;
+	END IF;
+END
+|
+DELIMITER ;
+
+#..........................................................................................
+
+DROP TRIGGER IF EXISTS add_salary_to_taxes;
+DELIMITER |
+CREATE TRIGGER add_salary_to_taxes AFTER INSERT ON salary_payments
+FOR EACH ROW
+BEGIN
+	DECLARE salary_amount FLOAT;
+
+	SET salary_amount = (SELECT amount
+						FROM salaries
+                        WHERE salaries.id = NEW.salary_id);
+
+	INSERT INTO taxes (issue_date, amount, type_id)
+    VALUES (NEW.date_payed, salary_amount, (SELECT id
+									  FROM tax_type
+                                      WHERE type = 'salaries'))
+	ON DUPLICATE KEY UPDATE
+    amount = amount + salary_amount;
+END
+|
+DELIMITER ;
+
+#..........................................................................................
